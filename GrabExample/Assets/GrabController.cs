ba7e9dcb1fafaf8108dc03df8529d2d5d6fd8781
WonSoEun <cutesoen@gmail.com>
2025-04-20 02:42:23 +0900

    VR 프로젝트 - 다양한 기능 구현

---
+++ GrabExample/Assets/GrabController.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class GrabController : MonoBehaviour
+{
+    public enum GrabState
+    {
+        ready,
+        inhand
+    }
+
+    public GrabState grabState;
+
+    [Header("VR Input")]
+    public SteamVR_Action_Boolean rightGrab;
+    public SteamVR_Action_Boolean b_Button;
+
+    private SteamVR_Input_Sources rHand;
+    public SteamVR_Behaviour_Pose m_Pose;  
+
+    private RaycastHit hit;
+    private RaycastHit hit2;
+
+    [Header("Object")]
+    public GameObject grabMark;
+    public GameObject guideLaser;
+    public LayerMask layerMask;         //오브젝트의 레이어
+
+    public GameObject socket;           //잡힐 물건의 기본위치
+
+    private GameObject targetObject;    //잡혀질 물건이 될 오브젝트
+    private Rigidbody targetRigidbody;  
+
+    public GameObject handModel;
+
+    private MeshRenderer guideLaserMat;
+
+
+    [Header("Variable")]
+    public float maxDistance;
+    float adjustment_Distance;
+
+    
+
+    private void Start()
+    {
+        guideLaser.SetActive(false);
+        grabMark.SetActive(false);
+
+        grabState = GrabState.ready;
+
+        rHand = SteamVR_Input_Sources.RightHand;
+        guideLaserMat = guideLaser.transform.GetChild(0).GetComponent<MeshRenderer>();
+        
+    }
+
+    private void Update()
+    {
+        if (b_Button.GetState(rHand) && grabState == GrabState.ready)
+        {
+            guideLaser.SetActive(true);
+            ObjectGrabTarget();
+        }
+
+        if (b_Button.GetStateUp(rHand))
+        {
+            guideLaser.SetActive(false);
+            grabMark.SetActive(false);
+        }
+
+
+        if (rightGrab.GetStateUp(rHand) && grabState == GrabState.inhand)
+        {
+            if (targetRigidbody != null)
+            {
+                targetRigidbody.isKinematic = false;
+            }
+
+            targetRigidbody.velocity = m_Pose.GetVelocity();
+            targetRigidbody.angularVelocity = m_Pose.GetAngularVelocity();
+            targetObject.transform.parent = null;
+
+            handModel.SetActive(true);
+            targetObject = null;
+            targetRigidbody = null;
+            grabState = GrabState.ready;
+        }
+
+    }
+
+
+    private void ObjectGrabTarget()
+    {
+        Debug.DrawRay(transform.position, transform.forward * maxDistance, Color.blue, 0); //0 - 초단위로 보이게 함. 
+        Debug.DrawRay(transform.position, transform.forward * hit2.distance, Color.red, 0);
+
+
+        //광선에 닿았을 때
+        if (Physics.Raycast(transform.position, transform.forward, out hit, maxDistance, layerMask))
+        {
+            grabMark.transform.position = hit.transform.position;
+            guideLaserMat.material.color = Color.blue;
+            grabMark.SetActive(true);
+            
+            if(rightGrab.GetStateDown(rHand))
+            {
+                CatchObj();
+            }
+        }
+        else
+        {
+            guideLaserMat.material.color = Color.red;
+            grabMark.SetActive(false);
+        }
+
+        //광선에 닿았을 때 BaceLaser길이 조정
+        if (Physics.Raycast(transform.position, transform.forward, out hit2))
+        {
+            adjustment_Distance = hit2.distance;
+            guideLaser.transform.localScale = new Vector3(0.005f, 0.005f, adjustment_Distance);
+        }
+
+    }
+
+    public void CatchObj()
+    {
+        grabMark.SetActive(false);
+        targetObject = hit.transform.gameObject;
+        targetObject.transform.SetParent(transform.parent); // GrabManager의 자식객체 - targetObject
+
+        targetObject.transform.position = socket.transform.position;
+        targetObject.transform.rotation = socket.transform.rotation;
+
+        if (targetObject.GetComponent<Rigidbody>() != null)
+        {
+            targetRigidbody = targetObject.GetComponent<Rigidbody>();
+            targetRigidbody.isKinematic = true;
+        }
+
+        handModel.SetActive(false);
+        guideLaser.SetActive(false);
+        grabState = GrabState.inhand;
+    }
+
+}
---
+++ GrabExample/Assets/InventoryController.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class InventoryController : MonoBehaviour
+{
+    enum InventoryState
+    {
+        in_Inven,
+        out_Inven
+    }
+
+    private InventoryState invenState;
+
+    [Header("VR Input")]
+    public SteamVR_Action_Boolean rightGrab;
+    private SteamVR_Input_Sources rHand;
+
+    [Header("Object")]
+    public GameObject camera_Socket;
+    public GameObject gun;
+    public GameObject controller_Collider_Socket;
+    public GameObject controller_Collider_Obj;
+    public GameObject r_Controller_socket;
+
+    [Header("Material")]
+    public Material basicInventoryMat;
+
+    private MeshRenderer onInventoryMat;
+
+    private BoxCollider gun_Collider1;
+    private BoxCollider gun_Collider2;
+    private BoxCollider controller_Collider;
+
+    [Header("Script")]
+    public GrabController grabControll;
+
+
+    bool on_Inven = false;
+    bool out_Inven = false;
+
+    private void Start()
+    {
+
+        rHand = SteamVR_Input_Sources.RightHand;
+
+        onInventoryMat = gameObject.transform.GetComponent<MeshRenderer>();
+
+        gun_Collider1 = gun.transform.GetChild(0).GetChild(2).GetChild(0).GetComponent<BoxCollider>();
+        gun_Collider2 = gun.transform.GetChild(0).GetChild(2).GetChild(1).GetComponent<BoxCollider>();
+        controller_Collider = controller_Collider_Obj.GetComponent<BoxCollider>();
+    }
+
+    private void Update()
+    {
+        gameObject.transform.position = camera_Socket.transform.position;
+        gameObject.transform.rotation = camera_Socket.transform.rotation;
+
+        controller_Collider_Obj.transform.position = controller_Collider_Socket.transform.position;
+        controller_Collider_Obj.transform.rotation = controller_Collider_Socket.transform.rotation;
+
+        if (on_Inven && invenState == InventoryState.in_Inven)
+        {
+            gun.transform.position = gameObject.transform.position;
+            gun.transform.rotation = gameObject.transform.rotation;
+
+            onInventoryMat.material.color = Color.blue;
+
+            if (gun.GetComponent<Rigidbody>() != null)
+            {
+                gun.GetComponent<Rigidbody>().useGravity = false;
+            }
+
+            gun_Collider1.isTrigger = true;
+            gun_Collider2.isTrigger = true;
+
+        }
+        else
+        {
+            onInventoryMat.material = basicInventoryMat;
+
+            gun_Collider1.isTrigger = false;
+            gun_Collider2.isTrigger = false;
+
+        }
+
+        if (out_Inven)
+        {
+            controller_Collider.isTrigger = true;
+            gun.transform.position = r_Controller_socket.transform.position;
+            gun.transform.rotation = r_Controller_socket.transform.rotation;
+
+            grabControll.grabState = GrabController.GrabState.inhand;
+
+            grabControll.CatchObj();
+
+            if (rightGrab.GetStateUp(rHand))
+            {
+                gun.GetComponent<Rigidbody>().useGravity = true;
+                controller_Collider.isTrigger = false;
+                out_Inven = false;
+            }
+        }
+
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (rightGrab.GetStateUp(rHand) && other.CompareTag("gun") && grabControll.grabState == GrabController.GrabState.ready)
+        {
+            on_Inven = true;
+            invenState = InventoryState.in_Inven;
+        }
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("RHand_Controller") && rightGrab.GetState(rHand) && invenState == InventoryState.in_Inven)
+        {
+            out_Inven = true;
+            invenState = InventoryState.out_Inven;
+            on_Inven = false;
+        }
+    }
+}
